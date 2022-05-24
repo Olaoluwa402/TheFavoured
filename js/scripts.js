36603d5fe8d87154021e99041d240a86a5646b08
olaoluwa402 <ibukundaniel402@yahoo.co.uk>
2022-05-24 21:25:55 +0100

    remove cart functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,40 @@
-let removeCartItemButton = document.getElementsByClassName('cart-items-remove');
-for(let i=0; i<removeCartItemButton.length; i++){
-    const button = removeCartItemButton[i];
-          button.addEventListener('click', (e)=>{
-                let buttonClicked = e.target;
-                    buttonClicked.parentElement.parentElement.remove()
-                    updateCartTotal();
-          })
+
+if(document.readyState == 'loading'){
+    document.addEventListener('DOMContentLoaded', ready)
+}else{
+    ready()
 }
 
-function updateCartTotal(element){
-    
+const {log} = console;
+
+function ready(){
+    let removeCartItemButton = document.getElementsByClassName('cart-items-remove');
+    for(let i=0; i<removeCartItemButton.length; i++){
+    const button = removeCartItemButton[i];
+          button.addEventListener('click', removeCartItem)    
+        }
+}
+
+function removeCartItem(e){
+    let buttonClicked = e.target;
+    buttonClicked.parentElement.parentElement.remove()
+    updateCartTotal();
+}
+
+function updateCartTotal(){
+    let cart = document.getElementsByClassName('cart')[0];
+    let cartItems = cart.getElementsByClassName('cart-items')
+    let total = 0
+    for(let i=0; i<cartItems.length; i++){
+        const cartItem = cartItems[i]
+        const priceElement = cartItem.getElementsByClassName('cart-items-price')[0]
+        const qtyElement = cartItem.getElementsByClassName('cart-items-qty')[0]
+        const price = Number(priceElement.innerText.replace('₦', ''))
+        const qty = qtyElement.value
+
+          total = total + (price * qty)
+    }
+    document.getElementsByClassName('cart-total')[0].innerText = `Total: ₦${total}`
 }
 
 
